c81729add70155f47944761ede43b876c510367a
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2025-12-31 12:53:49 +0530

    feat: implement chat functionality in ChatBox, enhance Connections and CreatePost pages, and improve user profile display in Profile

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,37 +1,34 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar.jsx'
-import {Menu, X} from 'lucide-react'
-import {Outlet} from 'react-router-dom'
-import { dummyUserData } from '../assets/assets.js'
-import { useState } from 'react'
-import Loading from '../components/Loading.jsx'
-
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import { Outlet } from 'react-router-dom'
+import { Menu, X } from 'lucide-react'
+import { dummyUserData } from '../assets/assets'
+import Loading from '../components/Loading'
+import { useSelector } from 'react-redux'
 
 const Layout = () => {
 
-  const user= dummyUserData
-
-  // For opening and closing of sidebar
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+    const user = useSelector((state)=>state.user.value)
+    const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return user ? (
-    <div className='w-full flex h-full'>
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <div className='flex-1 bg-slate-50'>
-        <Outlet />                      {/*Loads different pages inside layout*/}                    
-      </div>
+    <div className='w-full flex h-screen'>
+
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+
+        <div className='flex-1 bg-slate-50'>
+            <Outlet />
+        </div>
       {
-        sidebarOpen ?
-        <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden'
-          onClick={() => setSidebarOpen(false)} />
-        :
-        <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden'
-          onClick={() => setSidebarOpen(true)} />
+        sidebarOpen ? 
+        <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(false)}/>
+        : 
+        <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(true)}/>
       }
     </div>
-  ) : 
+  ) : (
     <Loading />
-
+  )
 }
 
-export default Layout 
+export default Layout
