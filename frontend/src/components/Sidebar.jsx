5551a2c2f3021815532dff32ba8826a278142d02
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2026-01-22 10:54:21 +0530

    feat: replace dummy user data with Redux state for user information in Sidebar component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { assets, dummyUserData } from "../assets/assets";
+import { assets } from "../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
 import MenuItems from "./MenuItems";
 import { CirclePlus, LogOut } from "lucide-react";
 import { UserButton, useClerk } from "@clerk/clerk-react";
+import { useSelector } from "react-redux";
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
-  const user = dummyUserData;
+  const user = useSelector((state) => state.user.value);
   const { signOut } = useClerk();
   const navigate = useNavigate();
 
